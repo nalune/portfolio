651b6ee7437ce34101c0bdc561a212a35301ffcf
Daria Zolotko <nalune@gmail.com>
2015-04-29 23:59:38 -0700

    added header image

--- front-page.php
+++ front-page.php
@@ -29,6 +29,10 @@
 			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+                
+                <div id="logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" />
+                </div>
             
 		<div class="site-branding">
                         
