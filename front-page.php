238957ca5a22bc5d2383e25053539ec97bc260f0
Daria Zolotko <nalune@gmail.com>
2015-05-13 13:55:09 -0700

    added google analytics

--- front-page.php
+++ front-page.php
@@ -18,6 +18,17 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62949714-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <?php wp_head(); ?>
 </head>
 
